8be3f113d7831ae8726b81426324c8c246e59b2a
Dimitri Wulffert <dimknight@gmail.com>
2023-03-09 09:41:59 +0100

    Added more plugins

--- lua/kuma/packer.lua
+++ lua/kuma/packer.lua
@@ -57,4 +57,13 @@ use {
   'nvim-lualine/lualine.nvim',
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 }
+
+use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+
 end)
