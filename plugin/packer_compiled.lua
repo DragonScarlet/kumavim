306d66af2d1a765906db6c2f143e9600262ffe60
Dimitri Wulffert <dimknight@gmail.com>
2023-03-25 23:02:46 +0100

    Staring to add java lsp

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -155,6 +155,16 @@ _G.packer_plugins = {
     path = "/Users/U127215/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-dap"] = {
+    loaded = true,
+    path = "/Users/U127215/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
+  },
+  ["nvim-jdtls"] = {
+    loaded = true,
+    path = "/Users/U127215/.local/share/nvim/site/pack/packer/start/nvim-jdtls",
+    url = "https://github.com/mfussenegger/nvim-jdtls"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/Users/U127215/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
