e3c3d7a87c8addabeae42d69104e3c6a85723360
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-03-29 22:27:17 +0200

    Updated Mascot and added icons to lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -203,7 +203,7 @@ ins_left {
 ins_left {
     -- Lsp server name .
     function()
-        local msg = 'No Active Lsp'
+        local msg = '󰊗'
         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
         local clients = vim.lsp.get_active_clients()
         if next(clients) == nil then
@@ -212,12 +212,18 @@ ins_left {
         for _, client in ipairs(clients) do
             local filetypes = client.config.filetypes
             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                if client.name == 'jdtls' then
+                    return ''
+                end
+                if client.name == 'lua_ls' then
+                    return ''
+                end
                 return client.name
             end
         end
         return msg
     end,
-    icon = ' LSP:',
+    icon = '( ˘▽˘)っ',
     color = { fg = colors.blue, gui = 'bold' },
 }
 
