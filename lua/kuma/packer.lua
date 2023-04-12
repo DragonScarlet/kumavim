457286b0fc9dc8c9480a5fd8032267c2fa4eb068
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-04-12 22:56:30 +0200

    Added some epic plugins

--- lua/kuma/packer.lua
+++ lua/kuma/packer.lua
@@ -105,4 +105,20 @@ return require('packer').startup(function(use)
         }
     })
     use 'echasnovski/mini.nvim'
+    use({
+        "folke/noice.nvim",
+        config = function()
+            require("noice").setup({
+                -- add any options here
+            })
+        end,
+        requires = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    })
 end)
