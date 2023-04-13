257c859ce33affe33fb6c269a755e924bca8eb7e
Dimitri Wulffert <dimknight@gmail.com>
2023-04-13 23:07:47 +0200

    Updated settings

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,46 @@
+local jdtls = require('jdtls')
+--- Helper function for creating keymaps
+function nnoremap(rhs, lhs, bufopts, desc)
+    bufopts.desc = desc
+    vim.keymap.set("n", rhs, lhs, bufopts)
+end
+
+-- The on_attach function is used to set key maps after the language server
+-- attaches to the current buffer
+local on_attach = function(client, bufnr)
+    jdtls.setup_dap({ hotcodereplace = 'auto' })
+    -- Regular Neovim LSP client keymappings
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    nnoremap('gD', vim.lsp.buf.declaration, bufopts, "Go to declaration")
+    nnoremap('gd', vim.lsp.buf.definition, bufopts, "Go to definition")
+    nnoremap('gi', vim.lsp.buf.implementation, bufopts, "Go to implementation")
+    nnoremap('K', vim.lsp.buf.hover, bufopts, "Hover text")
+    nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts, "Show signature")
+    nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
+    nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
+    nnoremap('<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts, "List workspace folders")
+    nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts, "Go to type definition")
+    nnoremap('<space>rn', vim.lsp.buf.rename, bufopts, "Rename")
+    nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts, "Code actions")
+    vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
+        { noremap = true, silent = true, buffer = bufnr, desc = "Code actions" })
+    nnoremap('<space>f', function() vim.lsp.buf.format { async = true } end, bufopts, "Format file")
+
+    -- Java extensions provided by jdtls
+    nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+    nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
+    nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
+    vim.keymap.set('v', "<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+        { noremap = true, silent = true, buffer = bufnr, desc = "Extract method" })
+end
+
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+
 local config = {
     -- The command that starts the language server
+    on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
     -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
     cmd = {
 
@@ -47,6 +87,73 @@ local config = {
     -- for a list of options
     settings = {
         java = {
+            format = {
+                settings = {
+                    -- Use Google Java style guidelines for formatting
+                    -- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+                    -- and place it in the ~/.local/share/eclipse directory
+                    url = "/.local/share/eclipse/eclipse-java-google-style.xml",
+                    profile = "GoogleStyle",
+                },
+            },
+            signatureHelp = { enabled = true },
+            contentProvider = { preferred = 'fernflower' }, -- Use fernflower to decompile library code
+            -- Specify any completion options
+            completion = {
+                favoriteStaticMembers = {
+                    "org.hamcrest.MatcherAssert.assertThat",
+                    "org.hamcrest.Matchers.*",
+                    "org.hamcrest.CoreMatchers.*",
+                    "org.junit.jupiter.api.Assertions.*",
+                    "java.util.Objects.requireNonNull",
+                    "java.util.Objects.requireNonNullElse",
+                    "org.mockito.Mockito.*"
+                },
+                filteredTypes = {
+                    "com.sun.*",
+                    "io.micrometer.shaded.*",
+                    "java.awt.*",
+                    "jdk.*", "sun.*",
+                },
+            },
+            -- Specify any options for organizing imports
+            sources = {
+                organizeImports = {
+                    starThreshold = 9999,
+                    staticStarThreshold = 9999,
+                },
+            },
+            -- How code generation should act
+            codeGeneration = {
+                toString = {
+                    template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+                },
+                hashCodeEquals = {
+                    useJava7Objects = true,
+                },
+                useBlocks = true,
+            },
+            -- If you are developing in projects with different Java versions, you need
+            -- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
+            -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+            -- And search for `interface RuntimeOption`
+            -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+            configuration = {
+                runtimes = {
+                    {
+                        name = "JavaSE-1.8",
+                        path = "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home/",
+                    },
+                    {
+                        name = "JavaSE-11",
+                        path = "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home/",
+                    },
+                    {
+                        name = "JavaSE-17",
+                        path = "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/",
+                    },
+                }
+            }
         }
     },
     -- Language server `initializationOptions`
@@ -57,7 +164,11 @@ local config = {
     --
     -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
     init_options = {
-        bundles = {}
+        bundles = {
+            vim.fn.glob(
+                '/Users/U127215/javaDebug/java-debug-main/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'),
+
+        }
     },
 }
 -- This starts a new client & server,am
