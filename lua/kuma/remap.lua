9211c1fda7df54faf67c8d541cb61216f5e35557
Dimitri Wulffert <dimknight@gmail.com>
2023-04-26 14:56:59 +0200

    Remove shortcut for quit

--- lua/kuma/remap.lua
+++ lua/kuma/remap.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Exit
-vim.keymap.set("n", "<leader>q", ':q!<CR>')
+-- vim.keymap.set("n", "<leader>q", ':q!<CR>')
 vim.api.nvim_create_user_command('Qq', 'q! | q!', {})
