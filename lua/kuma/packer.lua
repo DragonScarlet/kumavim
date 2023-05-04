fb7c5a384ed0e13a4eccbf82c523c48fdcd74651
Dimitri Wulffert <dimknight@gmail.com>
2023-05-04 15:42:51 +0200

    Add codeium to packer

--- lua/kuma/packer.lua
+++ lua/kuma/packer.lua
@@ -121,5 +121,17 @@ return require('packer').startup(function(use)
         config = function()
             require('nvim-test').setup()
         end
+
+    }
+    use {
+        "jcdickinson/codeium.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({
+            })
+        end
     }
 end)
