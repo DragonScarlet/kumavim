58cca7eb912451b76110667a5dfc9cfbfbc3dd4e
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-05-31 12:27:36 +0200

    Add autoformat on command

--- lua/kuma/remap.lua
+++ lua/kuma/remap.lua
@@ -4,3 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Exit
 -- vim.keymap.set("n", "<leader>q", ':q!<CR>')
 vim.api.nvim_create_user_command('Qq', 'q! | q!', {})
+
+-- Autoformat
+vim.keymap.set("v", "<leader>f", ":'<, '> lua vim.lsp.buf.format() <CR>")
+vim.keymap.set("n", "<leader>ff", ":lua vim.lsp.buf.format() <CR>")
