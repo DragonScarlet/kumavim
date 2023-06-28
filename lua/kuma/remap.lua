626ed945077b493b0f8089555d17d48b1abf4dc7
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-06-28 07:39:35 +0200

    Refactor variables

--- lua/kuma/remap.lua
+++ lua/kuma/remap.lua
@@ -1,10 +1,12 @@
+local keymap = vim.keymap.set
+local command = vim.api.nvim_create_user_command
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap("n", "<leader>pv", vim.cmd.Ex)
 
 -- Exit
--- vim.keymap.set("n", "<leader>q", ':q!<CR>')
-vim.api.nvim_create_user_command('Qq', 'q! | q!', {})
+command('Qq', 'q! | q!', {})
 
 -- Autoformat
-vim.keymap.set("v", "<leader>f", ":'<, '> lua vim.lsp.buf.format() <CR>")
-vim.keymap.set("n", "<leader>ff", ":lua vim.lsp.buf.format() <CR>")
+keymap("v", "<leader>f", ":'<, '> lua vim.lsp.buf.format() <CR>")
+keymap("n", "<leader>ff", ":lua vim.lsp.buf.format() <CR>")
